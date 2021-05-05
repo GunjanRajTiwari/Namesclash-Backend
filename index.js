bccda66889793af0ba66e690675deff9e9b852bf
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-05-06 01:00:30 +0545

    bug fix 1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const socket = require("socket.io");
 
 const app = express();
-app.use(express.static("public"));
+app.use(express.static(__dirname + "public"));
 
 // Routes
 app.get("/", (req, res) => {
