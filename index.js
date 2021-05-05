359d538fbe5278014ab8fddb1494afb2fe40fa71
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-05-05 19:08:46 +0545

    frontend added and socket connected

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require("express");
+const socket = require("socket.io");
+
+const corsOptions = {
+    origin: "https://localhost:5500",
+};
 
 const app = express();
+app.use(express.static("public"));
 
-app.listen(process.env.PORT | 8080, () => {
+// Routes
+app.get("/", (req, res) => {
+    res.redirect("/chat.html");
+});
+
+// Listening to the server
+const server = app.listen(process.env.PORT | 8000, () => {
     console.log("Server is running ...");
+});
+
+// Socket Setup
+const io = socket(server);
+
+io.on("connection", (socket) => {
+    console.log("Made socket connection:", socket.id);
 });
