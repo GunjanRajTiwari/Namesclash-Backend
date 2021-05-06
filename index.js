744f6b5e13178c141aaf1e4fc7b561d5a0714ca7
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-05-06 10:31:29 +0545

    public added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const socket = require("socket.io");
 const path = require("path");
 
 const app = express();
-app.use(express.static("public"));
+app.use("/public", express.static("public"));
 
 // Routes
 app.get("/", (req, res) => {
