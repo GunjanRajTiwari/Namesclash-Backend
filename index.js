2c1eaf8347a881c7a06508d9509f84de65342d0f
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-05-06 09:43:30 +0545

    procfile added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const socket = require("socket.io");
 const path = require("path");
 
 const app = express();
-app.use(express.static(path.join(__dirname + "public")));
+app.use(express.static("public"));
 
 // Routes
 app.get("/", (req, res) => {
