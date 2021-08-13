8fb35d4746ca6d43d6f901a8bfbeccd0fe820c4b
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-08-13 16:52:06 +0545

    💣 Notification on message

--- views/js/profile.js
+++ views/js/profile.js
@@ -48,9 +48,11 @@ var btn = document.getElementById("select-gang");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function () {
-	modal.style.display = "block";
-};
+if (btn) {
+	btn.onclick = function () {
+		modal.style.display = "block";
+	};
+}
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
