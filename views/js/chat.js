167ea65a3b1952ed1f64159de5a7b2019f79449d
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-08-13 18:40:49 +0545

    🔥  storing messages temporarily

--- views/js/chat.js
+++ views/js/chat.js
@@ -10,58 +10,76 @@ const feedback = document.getElementById("typing");
 
 // Emit events
 function fireMessage() {
-    if (!message.value) {
-        return;
-    }
+	if (!message.value) {
+		return;
+	}
 
-    socket.emit("chat", {
-        id: user.googleId,
-        name: user.name,
-        photo: user.photoUrl,
-        gang: user.gang,
-        message: message.value,
-    });
+	socket.emit("chat", {
+		id: user.googleId,
+		name: user.name,
+		photo: user.photoUrl,
+		gang: user.gang,
+		message: message.value,
+	});
 
-    message.value = "";
+	message.value = "";
+}
+
+// Load Messages
+const oldMsg = localStorage.getItem("ncMsg");
+if (oldMsg) {
+	oldMsg.map(data => {
+		addMessages(data);
+	});
 }
 
 // Event handlers
 btn.addEventListener("click", fireMessage);
-message.addEventListener("keyup", (e) => {
-    if (e.key === "Enter") {
-        e.preventDefault();
-        fireMessage();
-    }
+message.addEventListener("keyup", e => {
+	if (e.key === "Enter") {
+		e.preventDefault();
+		fireMessage();
+	}
 });
 
 message.addEventListener("keypress", function () {
-    socket.emit("typing", {
-        gang: user.gang,
-        value: "Someone is typing ...",
-    });
+	socket.emit("typing", {
+		gang: user.gang,
+		value: "Someone is typing ...",
+	});
 });
 
 socket.emit("joinRoom", user);
 
-// Listen for events
-socket.on("chat", (data) => {
-    const div = document.createElement("div");
-    div.classList.add("chat");
-    if (data.id === user.googleId) {
-        div.classList.add("mine");
-    }
-    div.innerHTML = `
+const addMessages = data => {
+	const div = document.createElement("div");
+	div.classList.add("chat");
+	if (data.id === user.googleId) {
+		div.classList.add("mine");
+	}
+	div.innerHTML = `
         <div class="head">
         <img class="avatar" src="${data.photo}">
         <small>${data.name}</small>
         <div>
         <p class="message-text">${data.message}</p>
     `;
-    output.appendChild(div);
-    output.scrollTop = output.scrollHeight;
-    feedback.innerHTML = "";
+	output.appendChild(div);
+	output.scrollTop = output.scrollHeight;
+	feedback.innerHTML = "";
+};
+
+// Listen for events
+socket.on("chat", data => {
+	addMessages(data);
+	const msgStore = localStorage.getItem("ncMsg") || [];
+	msgStore.push(data);
+	if (msgStore.length > 20) {
+		msgStore.shift();
+	}
+	localStorage.setItem("ncMsg", msgStore);
 });
 
 socket.on("typing", function (data) {
-    feedback.innerHTML = `<small>${data}</small>`;
+	feedback.innerHTML = `<small>${data}</small>`;
 });
